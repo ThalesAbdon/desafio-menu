0b21774f4b5bd812ab909fa7cfeebf9026c3b6df
Thales <thales.sousufpi@gmail.com>
2026-07-15 20:05:23 -0300

    feat: add Menu controller, remove User controller

---
+++ backend-desafio/src/controllers/menu.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import MenuService from '../services/menu'
+
+export default {
+    create: async (req: Request, res: Response): Promise<Response> => {
+        try {
+            const { id } = await MenuService.create(req.body)
+            return res.status(201).json({ id })
+        } catch (err: any) {
+            if (err.message === 'DUPLICATE_NAME')
+                return res.status(400).json({ message: 'Name must be unique' })
+            throw err
+        }
+    },
+    delete: async (req: Request, res: Response): Promise<Response> => {
+        const result = await MenuService.delete(req.params.id)
+        if (!result) return res.status(400).json({ message: 'Item not found' })
+
+        return res.status(200).json({ id: result.id })
+    },
+    getAll: async (_: Request, res: Response): Promise<Response> => {
+        const tree = await MenuService.getAll()
+        return res.status(200).json(tree)
+    },
+}
