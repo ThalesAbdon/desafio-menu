63bfee6b55d9a162b8ba77cb0874f800898b7d74
Thales <thales.sousufpi@gmail.com>
2026-07-15 20:14:21 -0300

    refactor: wrap Menu routes with async handler

--- backend-desafio/src/routes/menu.ts
+++ backend-desafio/src/routes/menu.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import Menu from '../controllers/menu'
 import validation from '../validation/menu'
+import asyncHandler from '../middlewares/async-handler'
 
 const routes = express.Router()
 
-routes.post('/', validation.create, Menu.create)
-routes.delete('/:id', validation.objectId, Menu.delete)
-routes.get('/', Menu.getAll)
+routes.post('/', validation.create, asyncHandler(Menu.create))
+routes.delete('/:id', validation.objectId, asyncHandler(Menu.delete))
+routes.get('/', asyncHandler(Menu.getAll))
 
 export default routes
